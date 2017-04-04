fed72e75ae324e678347ad69e9f76e881091bec8
Philipp Scheit <p.scheit@ps-webforge.com>
2017-04-04 12:17:51 +0200

    add invoice id to expense

--- lib/FastBill/Model/Expense.php
+++ lib/FastBill/Model/Expense.php
@@ -7,6 +7,7 @@ class Expense extends AbstractModel
     protected static $xmlProperties = array(
         'ORGANIZATION' => 'organization',
 
+        'INVOICE_ID' => 'invoiceId',
         'INVOICE_DATE' => 'invoiceDate',
         'INVOICE_NUMBER' => 'invoiceNumber',
         'DUE_DATE' => 'dueDate',
